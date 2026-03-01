b26fe563f28114a5be0031ef566e47e30e0c2b82
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2026-03-01 13:59:19 +0200

    Delete filters directory

--- filters/admin.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters import BaseFilter
-from aiogram.types import Message
-
-from config import ADMIN_ID
-
-
-class IsAdmin(BaseFilter):
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id == int(ADMIN_ID)
